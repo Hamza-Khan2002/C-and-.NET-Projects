b8b8cd687e97ea414f714f397fa70f03f3fd6cc0
Hamza-Khan2002 <hk2651675@gmail.com>
2026-05-30 02:09:03 +0500

    Restructured: Removed loose files, Added SignUp-SignIn-Form

---
+++ SignUp-SignIn-Form/SignUp & SignIn Form/Middleware/JwtMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace SignUp___SignIn_Form.Middleware
+{
+    public class JwtMiddleware(RequestDelegate next, IConfiguration configuration)
+    {
+        private readonly RequestDelegate _next = next;
+        private readonly IConfiguration _configuration = configuration;
+
+        private void AttachUserToContext(HttpContext context, string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(
+                    _configuration.GetSection("JwtConfig")
+                    .GetSection("Secret").Value!);
+
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var userId = int.Parse(jwtToken.Claims.First(x => x.Type == "nameid").Value);
+                    context.Items["UserId"] = userId;
+            }
+            catch { }
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"]
+                .FirstOrDefault()?
+                .Split(" ")
+                .Last();
+
+            if (token != null) AttachUserToContext(context, token);
+
+            await _next(context);
+        }
+
+    }    
+}
---
+++ SignUp-SignIn-Form/SignUp & SignIn Form/Repository/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SignUp___SignIn_Form.Data;
+using SignUp___SignIn_Form.Interfaces;
+using SignUp___SignIn_Form.Models;
+
+namespace SignUp___SignIn_Form.Repository
+{
+    public class UserRepository(ApplicationDbContext context) : IUserRepository
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public async Task<User> CreateUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            ArgumentNullException.ThrowIfNull(email);
+
+            return await _context.Users.
+                AsNoTracking().
+                FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User?> GetUserByIdAsync(int id)
+        {
+            return await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> UserExistance(string email)
+        {
+            return await _context.Users.AnyAsync(u => u.Email == email);
+        }
+
+    }
+}
