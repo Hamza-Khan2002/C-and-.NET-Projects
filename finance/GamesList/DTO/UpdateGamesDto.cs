e2adec4b03b2d93b1d488af335d4a46953454126
Hamza-Khan2002 <hk2651675@gmail.com>
2026-06-04 01:48:16 +0500

    Finance Project In Progress

--- finance/GamesList/DTO/UpdateGamesDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GamesList.DTO
-{
-    public class UpdateGamesDto
-    {
-        public string Name { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-        public List<int> GenreIds { get; set; } = [];
-    }
-}
