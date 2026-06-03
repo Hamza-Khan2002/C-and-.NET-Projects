0bfc6b5ab4717817039b1e45db214e525145f30d
Hamza-Khan2002 <hk2651675@gmail.com>
2026-06-04 01:42:11 +0500

    Fixed: Removed submodule, added finance as regular folder

---
+++ finance/GamesList/DTO/CreateGamesDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamesList.DTO
+{
+    public class CreateGamesDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public DateOnly CreatedOn { get; set; }
+        public List<int> GenreIds { get; set; } = [];
+    }
+}
