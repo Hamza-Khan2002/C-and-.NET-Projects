34802151e56af9758f2a1f81add20d2982c3a1ca
Hamza-Khan2002 <hk2651675@gmail.com>
2026-06-10 00:47:50 +0500

    Changes Updated

---
+++ FinanceProject/FinanceProject/Interfaces/ICommentRepository.cs
@@ -0,0 +1,10 @@
+﻿using FinanceProject.DTO.Comment;
+
+namespace FinanceProject.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<List<CommentDto>> GetAllCommentAsync();
+        Task<CommentDto> GetCommentByIdAsync(int id);
+    }
+}
---
+++ FinanceProject/FinanceProject/Interfaces/IStockRepository.cs
@@ -0,0 +1,13 @@
+﻿using FinanceProject.DTO.Stock;
+
+namespace FinanceProject.Interfaces
+{
+    public interface IStockRepository
+    {
+        Task<List<StockDto>> GetStocksAsync();
+        Task<StockDto> GetStockByIdAsync(int id);
+        Task<StockDto> CreateStockAsync(CreateStockDto data);
+        Task<StockDto> UpdateStockAsync(int id, UpdateStockDto data);
+        Task DeleteStockAsync(int id);
+    }
+}
